f520bb519d6071ff4e44ef6dc246b47e6c14e561
aiktcprogrammersclub@gmail.com <aiktcprogrammersclub@gmail.com>
2015-03-09 14:14:04 +0530

    uploaded group 8

---
+++ footer.php
@@ -0,0 +1,25 @@
+<html >
+<head>
+<title>Drug Free Life</title>
+<meta  charset='UTF-8' />
+<link rel='stylesheet' type='text/css' href='style.css' />
+<script type='text/javascript' src='js/boxOver.js'></script>
+</head>
+<body>
+<?php
+
+echo "<div id='main_container'>
+<div class='footer' align='center'>
+    <div class='left_footer'> <img src='images/logo.jpeg' alt='' width='89' height='42'/> </div>
+    <div class='center_footer'><a href =index.php> Drug Free Life</a><br />
+      <a href='http://csscreme.com'><img src='images/csscreme.jpg' alt='csscreme' title='csscreme' border='0' /></a><br />
+      <img src='images/payment.gif' alt='' /> </div>
+    <div class='right_footer'> <a href=' '>Home</a> <a href=' '>Drug of abuse</a> <a href=' '>News & Events</a> <a href=' '>Help Center</a><a href=' '>About DFL</a> <a href=' '>Contact Us</a> 
+  </div>
+</div>
+</div>
+<!-- end of main_container -->
+";
+?>
+</body>
+  </html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html >
+<head>
+<title>Drug Free Life</title>
+<meta  charset="UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<!--[if IE 6]>
+<link rel="stylesheet" type="text/css" href="iecss.css" />
+<![endif]-->
+<script type="text/javascript" src="js/boxOver.js"></script>
+</head>
+<body>
+<?php
+
+echo '<div id="main_container">
+  <div id="header">
+    <div class="top_right">
+      <div class="languages">
+        
+        </div>
+      <div class="big_banner"> <h1>Drug Free Life </h1> </div>
+    </div>
+    <div id="logo"> <a href="#"><img src="images/logo.jpeg" alt="" border="0" width="182" height="85" /></a> </div>
+  </div>
+  <div id="main_content">
+    <div id="menu_tab">
+      <ul class="menu">
+        <li><a href="#" class="nav"> Home </a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav">Drug of Abuse</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav">News & Events</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav">Help Center</a></li>
+        <li class="divider"></li>
+        <li><a href="#" class="nav">About DFL</a></li>
+        <li class="divider"></li>
+        <li><a href="contact.html" class="nav">Contact Us</a></li>
+        
+        
+     <div id="menu_right">   <li><a href="loginuser.php" class="nav">Login</a></li></div>
+         </ul>
+    </div>
+    
+    </div>';
+    ?>
+<!-- end of main_container -->
+<div align=center>This template  downloaded form <a href='#'>free website templates</a></div></body>
+</html>
